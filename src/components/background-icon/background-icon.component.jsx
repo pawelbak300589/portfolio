27d46c407d509ab36dad8d7885ba82cc40c24b07
Pawel Bak <pawelbak300589@gmail.com>
2020-07-01 16:48:23 +0100

    Update background icons component with some updates in css

--- src/components/background-icon/background-icon.component.jsx
+++ src/components/background-icon/background-icon.component.jsx
@@ -4,14 +4,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const BackgroundIcon = ({ icon, style, variants, transition }) => {
     const initialVariants = {
-        animate: { opacity: [0.6, 0.1, 0.6, 0.1, 0.6], y: ["0vh", "110vh", "0vh"] },
+        animate: {
+            opacity: [0.6, 0.6, 0.1, 0.6, 0.6, 0.6, 0.1, 0.6, 0.6],
+            scale: [1, 1, 0.7, 1, 1, 1, 0.7, 1, 1],
+            y: ["0vh", "115vh", "0vh"]
+        },
         ...variants
     };
     return (
         <motion.span
             style={{
                 position: "absolute",
-                top: "-10vh",
+                top: "-15vh",
                 left: "10%",
                 fontSize: 50,
                 ...style
--- src/components/background-icons/background-icons.component.jsx
+++ src/components/background-icons/background-icons.component.jsx
@@ -8,15 +8,15 @@ import './background-icons.styles.scss';
 const BackgroundIcons = () => {
     return (
         <div className="home-background">
-            <BackgroundIcon icon={faJsSquare} style={{ left: "9%", fontSize: "5vw" }} transition={{ duration: 15 }} />
-            <BackgroundIcon icon={faReact} style={{ left: "18%", fontSize: "5vw" }} transition={{ duration: 20 }} />
-            <BackgroundIcon icon={faPhp} style={{ left: "27%", fontSize: "5vw" }} transition={{ duration: 18 }} />
-            <BackgroundIcon icon={faCss3Alt} style={{ left: "36%", fontSize: "5vw" }} transition={{ duration: 28 }} />
+            <BackgroundIcon icon={faJsSquare} style={{ left: "5%", fontSize: "5vw" }} transition={{ duration: 15 }} />
+            <BackgroundIcon icon={faReact} style={{ left: "15%", fontSize: "5vw" }} transition={{ duration: 20 }} />
+            <BackgroundIcon icon={faPhp} style={{ left: "25%", fontSize: "5vw" }} transition={{ duration: 18 }} />
+            <BackgroundIcon icon={faCss3Alt} style={{ left: "35%", fontSize: "5vw" }} transition={{ duration: 28 }} />
             <BackgroundIcon icon={faSass} style={{ left: "45%", fontSize: "5vw" }} transition={{ duration: 25 }} />
-            <BackgroundIcon icon={faHtml5} style={{ left: "54%", fontSize: "5vw" }} transition={{ duration: 14 }} />
-            <BackgroundIcon icon={faLaravel} style={{ left: "63%", fontSize: "5vw" }} transition={{ duration: 21 }} />
-            <BackgroundIcon icon={faDocker} style={{ left: "72%", fontSize: "5vw" }} transition={{ duration: 18 }} />
-            <BackgroundIcon icon={faGitAlt} style={{ left: "81%", fontSize: "5vw" }} transition={{ duration: 22 }} />
+            <BackgroundIcon icon={faHtml5} style={{ left: "55%", fontSize: "5vw" }} transition={{ duration: 14 }} />
+            <BackgroundIcon icon={faLaravel} style={{ left: "65%", fontSize: "5vw" }} transition={{ duration: 21 }} />
+            <BackgroundIcon icon={faDocker} style={{ left: "75%", fontSize: "5vw" }} transition={{ duration: 18 }} />
+            <BackgroundIcon icon={faGitAlt} style={{ left: "85%", fontSize: "5vw" }} transition={{ duration: 22 }} />
         </div>
     );
 };
