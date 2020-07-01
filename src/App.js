7051f30409fa4a84eac6e84da762f44d21465247
Pawel Bak <pawelbak300589@gmail.com>
2020-07-01 10:37:30 +0100

    Add page transition with framer motion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Router, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 import AboutPage from "./pages/about/about.component";
 import HomePage from "./pages/home/home.component";
@@ -11,16 +12,20 @@ import SideMenu from "./components/side-menu/side-menu.component";
 import './App.css';
 
 function App() {
+    const location = useLocation();
+
     return (
         <div className="App">
             <SideMenu />
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route exact path="/about" component={AboutPage} />
-                <Route exact path="/skills" component={SkillsPage} />
-                <Route exact path="/portfolio" component={PortfolioPage} />
-                <Route exact path="/contact" component={ContactPage} />
-            </Switch>
+            <AnimatePresence>
+                <Switch location={location} key={location.pathname}>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/about" component={AboutPage} />
+                    <Route exact path="/skills" component={SkillsPage} />
+                    <Route exact path="/portfolio" component={PortfolioPage} />
+                    <Route exact path="/contact" component={ContactPage} />
+                </Switch>
+            </AnimatePresence>
         </div>
     );
 }
--- src/components/content/content.component.jsx
+++ src/components/content/content.component.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { motion } from "framer-motion";
+
+import { pageTransition, pageVariants } from "../../helpers/page-transition.helper";
 
 import './content.styles.scss';
 
 const Content = ({ children, className }) => {
     return (
-        <main className={className}>
+        <motion.main
+            className={className}
+            initial="initial"
+            animate="in"
+            exit="out"
+            variants={pageVariants}
+            transition={pageTransition}>
             {children}
-        </main>
+        </motion.main>
     );
 };
 
